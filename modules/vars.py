36363c2452ce1be0d26c460ec5d625640d0f88fa
Shivamparashar23 <146502436+Shivamparashar23@users.noreply.github.com>
2025-03-27 12:19:17 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,9 +1,9 @@
 from os import getenv
 
-api_id    = int(getenv("api_id", ""))
-api_hash  = getenv("api_hash", "")
-bot_token = getenv("bot_token", "")
-pwtoken = getenv("pwtoken", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-AUTH = ""
-#Bot Username = ""
+api_id    = int(getenv("api_id", "22116182"))
+api_hash  = getenv("api_hash", "f0cc1004236032532c3868b7ccb79bf3")
+bot_token = getenv("bot_token", "7256931543:AAG2fG6KfbG_t170UblHa0EiaQ-3TdTM8lU")
+pwtoken = getenv("pwtoken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NDM2NjI3MDQuOTkyLCJkYXRhIjp7Il9pZCI6IjY2MGY3N2EwMDRhNTk0MDAxODQ4MTFjZiIsInVzZXJuYW1lIjoiOTM1Mjc0MTA4MyIsImZpcnN0TmFtZSI6IlNoaXZhIiwibGFzdE5hbWUiOiIiLCJvcmdhbml6YXRpb24iOnsiX2lkIjoiNWViMzkzZWU5NWZhYjc0NjhhNzlkMTg5Iiwid2Vic2l0ZSI6InBoeXNpY3N3YWxsYWguY29tIiwibmFtZSI6IlBoeXNpY3N3YWxsYWgifSwiZW1haWwiOiJqZ2poZ2g1eXlAc2tpZmYuY29tIiwicm9sZXMiOlsiNWIyN2JkOTY1ODQyZjk1MGE3NzhjNmVmIl0sImNvdW50cnlHcm91cCI6IklOIiwidHlwZSI6IlVTRVIifSwiaWF0IjoxNzQzMDU3OTA0fQ.INZTmxvxG1erh0ebFHyrKsWTLQpazBUlHmmXxx6s5tk")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002619255560"))
+AUTH = "1768111749"
+#Bot Username = "djhdsbbot"
